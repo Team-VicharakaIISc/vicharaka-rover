83dd041dea96de500d654715dfcee602c42b2046
Pritesh Jogdhankar <129420569+pritesh299@users.noreply.github.com>
2024-10-19 00:24:10 +0530

    created path variables in launch file

--- launch/rsp.launch.py
+++ launch/rsp.launch.py
@@ -8,4 +8,19 @@ import xacro
 
 def generate_launch_description():
 
-    return LaunchDescription([])
+    # Check if we're told to use sim time
+    use_sim_time = LaunchConfiguration('use_sim_time')
+
+    #variables 
+    package__path=os.path.join(get_package_share_directory("vicharaka_rover"))
+
+    xacro_file_path=os.path.join(package__path,"description","rover.urdf.xacro")
+
+    rover_description_config= xacro.process_file(xacro_file_path)
+
+    return LaunchDescription([
+            DeclareLaunchArgument(
+                'use_sim_time',
+                default_value='false',
+                description='Use sim time if true'),
+    ])
